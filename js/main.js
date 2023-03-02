9597c956af341e2d3f040b7e7207585254b6722b
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-02 17:25:28 +0100

    add event on click for cell and play button

--- js/main.js
+++ js/main.js
@@ -4,15 +4,22 @@
 //   =>Generare con un ciclo dei div con classe dedicata, questi saranno le singole caselle. Ogni casella conterra un evento in ascolto per reagire al click  
 //   =>passare alla funzione che genera i div il numero di div che deve generare  
 
-
+//Element from page
 const board = document.getElementById('board');
+const play = document.getElementById('play');
 
+/*
+*  Function
+*/
 function createElement(htmlElement, htmlId, htmlClass, where, insertText) {
     const single = document.createElement(htmlElement);
     single.id = htmlId;
     single.className = htmlClass;
     single.innerText = insertText;
     where.appendChild(single);
+    single.addEventListener('click', function(){
+        single.classList.add('selected');
+    })
 }
 
 function gridDimension(colRowNumber){
@@ -22,5 +29,10 @@ function gridDimension(colRowNumber){
     }
 }
 
-
 // createElement('div', 'cell', 'cell-10', board, cell);
+
+
+/*
+*  Main
+*/
+
