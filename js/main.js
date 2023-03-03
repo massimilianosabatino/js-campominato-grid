0ea9b98de6c8a7aeab10197a5e9ea26fe977193e
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-03 18:56:03 +0100

    code and layout fix

--- js/main.js
+++ js/main.js
@@ -12,29 +12,37 @@ const select = document.getElementById('set-game');
 /*
 *  Function
 */
+//Create single cell
 function createElement(htmlElement, htmlId, htmlClass, where, insertText) {
     const single = document.createElement(htmlElement);
     single.id = htmlId;
     single.className = htmlClass;
     single.innerText = insertText;
     where.appendChild(single);
-    single.addEventListener('click', function(){
-        single.classList.add('selected');
-    })
+    single.addEventListener('click', function() {
+        playLogic(single);
+    });
 }
 
+//Create general board
 function gridDimension(colRowNumber){
     const totalCell = colRowNumber * colRowNumber;
+    let single = '';
     for(let cell = 1; cell <= totalCell; cell++){
         createElement('div', 'cell', `cell-${colRowNumber}`, board, cell);
     }
 }
 
+//Add gameplay logic
+function playLogic(single){
+        single.classList.add('selected');
+}
+
+//Start game and refresh grid
 function playGame(){
     board.innerHTML = '';
     gridDimension(select.value);
 }
-
 
 
 /*
